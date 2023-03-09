e4715b0cce21ee6b27bef403cd19e27d2260fcd5
Joan Ndegwa <44367535+jnjerin@users.noreply.github.com>
2023-03-09 23:53:21 +0300

    Create 5-sqrt_recursion.c

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,33 @@
+#include "holberton.h"
+
+int counter(int n, int x);
+
+/**
+ * _sqrt_recursion - return natural square root of number
+ * @n: number
+ *
+ * Return: natural square root on Success, -1 on Fail
+ */
+int _sqrt_recursion(int n)
+{
+	int x = 0;
+
+	return (counter(n, x));
+}
+
+/**
+ * counter - compare possible square roots of n
+ * @n: number
+ * @x: multiple to test
+ *
+ * Return: square root on Success, -1 on Fail
+ */
+int counter(int n, int x)
+{
+	if (x * x > n)
+		return (-1);
+	if (x * x == n)
+		return (x);
+
+	return (counter(n, x + 1));
+}
