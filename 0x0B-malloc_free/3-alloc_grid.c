30bd7fd0a99091a3c1569db562a9b494ced694a5
Joan Ndegwa <44367535+jnjerin@users.noreply.github.com>
2023-03-23 22:25:36 +0300

    Create 3-alloc_grid.c

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,46 @@
+#include "holberton.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * alloc_grid - initialize 2D array
+ * @width: array width
+ * @height: array height
+ *
+ * Return: pointer to 2D array on Success, NULL on Fail
+ */
+int **alloc_grid(int width, int height)
+{
+	int **grid;
+	int i, j;
+
+	/* check if dimensions 1 or greater */
+	if (width < 1 || height < 1)
+		return (NULL);
+
+	/* allocate memory for grid (pointer to pointer) */
+	grid = malloc(sizeof(int *) * height);
+	if (grid == NULL)
+		return (NULL);
+
+	/* allocate memory for *grid (pointer to int) */
+	for (i = 0; i < height; i++)
+	{
+		*(grid + i) = malloc(sizeof(int) * width);
+		if (*(grid + i) == NULL)
+		{
+			for (j = 0; j < i; j++)
+			{
+				free(grid);
+				free(*(grid + j));
+			}
+			return (NULL);
+		}
+	}
+
+	/* initialize all elements to 0 */
+	for (i = 0; i < height; i++)
+		for (j = 0; j < width; j++)
+			grid[i][j] = 0;
+	return (grid);
+}
