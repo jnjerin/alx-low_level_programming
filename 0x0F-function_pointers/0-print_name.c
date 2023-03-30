d469ed13170f55e317c785fc6360433525dc563c
Joan Ndegwa <44367535+jnjerin@users.noreply.github.com>
2023-03-30 23:53:10 +0300

    Create 0-print_name.c

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - print name
+ * @name: name to print
+ * @f: pointer to funtion
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name != NULL && f != NULL)
+		f(name);
+}
